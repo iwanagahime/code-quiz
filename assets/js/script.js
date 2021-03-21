234fb1bc97f01f8668f3a6ee985cc2309df79acc
iwanagahime <eliza.krucon@gmail.com>
2021-03-21 18:56:12 +0000

    add constructQuizContainer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 const startButtonElement = document.getElementById("start-quiz-button");
 
-const startQuiz = () => {};
+const constructQuizContainer = () => {
+  const quizContainerDiv = document.createElement("div");
+  quizContainerDiv.setAttribute("class", "quiz-container");
+};
+
+const startQuiz = () => {
+  // replace the start-quiz-section div with the quiz-container div
+
+  // construct the quiz-container div in JS
+  const quizDivElement = constructQuizContainer();
+};
 
 startButtonElement.addEventListener("click", startQuiz);
