5e7fe3aa32eb26e93ef4dee8f219a2a0c80129df
iwanagahime <eliza.krucon@gmail.com>
2021-03-23 23:31:45 +0000

    add timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 const startButtonElement = document.getElementById("start-quiz-button");
 const startQuizDiv = document.getElementById("start-quiz-section");
+const timerSpanElement = document.getElementById("timer-span");
 const bodyElement = document.body;
+
+// const quizCard = "questionAndAnswers";
+let timerValue = 60;
+
+// TODO how to construct questions and answers for quiz card
+const constructQuizCard = () => {
+  // const question =
+  // const answers =
+};
 
 const constructQuizContainer = () => {
   const quizContainerDiv = document.createElement("div");
@@ -11,17 +21,33 @@ const constructQuizContainer = () => {
     "question-and-answers-container"
   );
 
+  constructQuizCard();
   // TODO content of this container needs to be put in place
-  // questionAndAnswersContainer
+  // questionAndAnswersContainerDiv
 
   quizContainerDiv.appendChild(questionAndAnswersContainerDiv);
 
   return quizContainerDiv;
 };
 
+const startTimer = () => {
+  const timerTick = () => {
+    timerSpanElement.textContent = timerValue;
+    timerValue -= 1;
+
+    if (timerValue < 0) {
+      clearInterval(timer);
+    }
+  };
+  const timer = setInterval(timerTick, 1000);
+};
+
+// construct form container
+//  remove quiz container div and append form container div to body
+
 const startQuiz = () => {
   // replace the start-quiz-section div with the quiz-container div
-  // construct the quiz-container div in JS
+
   const quizDivElement = constructQuizContainer();
 
   // remove start-quiz section
@@ -31,9 +57,9 @@ const startQuiz = () => {
   // insert the quiz-container div
 
   bodyElement.appendChild(quizDivElement);
-  //  => replace;
-  // document.getElementById(startQuizSection);
-  // document.getElementById(quizContainerDiv);
+
+  // start timer here
+  startTimer();
 };
 console.log("Start Quiz");
 
