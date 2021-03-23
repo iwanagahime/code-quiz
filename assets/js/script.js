25d91ee72e589976830b07b9ba81e046bdb3fcbe
iwanagahime <eliza.krucon@gmail.com>
2021-03-23 17:36:03 +0000

    declare body Element const

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 const startButtonElement = document.getElementById("start-quiz-button");
 const startQuizDiv = document.getElementById("start-quiz-section");
+const bodyElement = document.body;
 
 const constructQuizContainer = () => {
   const quizContainerDiv = document.createElement("div");
   quizContainerDiv.setAttribute("class", "quiz-container");
-  const questionAndAnswersContainer = document.createElement("div");
-  questionAndAnswersContainer.setAttribute(
+  const questionAndAnswersContainerDiv = document.createElement("div");
+  questionAndAnswersContainerDiv.setAttribute(
     "class",
     "question-and-answers-container"
   );
@@ -13,7 +14,7 @@ const constructQuizContainer = () => {
   // TODO content of this container needs to be put in place
   // questionAndAnswersContainer
 
-  quizContainerDiv.appendChild(questionAndAnswersContainer);
+  quizContainerDiv.appendChild(questionAndAnswersContainerDiv);
 
   return quizContainerDiv;
 };
@@ -25,9 +26,11 @@ const startQuiz = () => {
 
   // remove start-quiz section
 
-  document.body.removeChild(startQuizDiv);
+  bodyElement.removeChild(startQuizDiv);
 
-  document.body.appendChild(quizDivElement);
+  // insert the quiz-container div
+
+  bodyElement.appendChild(quizDivElement);
   //  => replace;
   // document.getElementById(startQuizSection);
   // document.getElementById(quizContainerDiv);
