6eba9e48e361b78e8c2443a709679bf3311162b8
iwanagahime <eliza.krucon@gmail.com>
2021-03-23 15:11:25 +0000

    create quiz and answer container divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const startButtonElement = document.getElementById("start-quiz-button");
 const constructQuizContainer = () => {
   const quizContainerDiv = document.createElement("div");
   quizContainerDiv.setAttribute("class", "quiz-container");
+  const answersContainerDiv = document.createElement("div");
   answersContainerDiv.setAttribute("class", "answers-container");
 
   //TODO how to set the buttons for answers within this answer div?
@@ -12,7 +13,9 @@ const startQuiz = () => {
   // replace the start-quiz-section div with the quiz-container div
 
   // construct the quiz-container div in JS
-  const quizDivElement = constructQuizContainer();
+  quizDivElement = constructQuizContainer = () => replace;
+  document.getElementById(quizContainerDiv);
+  document.getElementById(answersContainerDiv);
 };
 
 startButtonElement.addEventListener("click", startQuiz);
